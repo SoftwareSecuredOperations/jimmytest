6db8aec422731fe4b7d701d6741e013c6056c87c
SoftwareSecuredOperations <159371968+SoftwareSecuredOperations@users.noreply.github.com>
2024-12-10 14:54:16 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,5 @@ import sonrai.platform.aws.arn
 
 log = logging.getLogger()
 
-# <img src=1>
-# Stop AWS EC2 Instance : AWS ref(https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Client.stop_instances)
-
 def run(ctx):
-
-  # <img src=1>
   pass
