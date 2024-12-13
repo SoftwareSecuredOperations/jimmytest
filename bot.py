e3bba60f8d3c3853f833d43f75e6f1ee9ed799c7
SoftwareSecuredOperations <159371968+SoftwareSecuredOperations@users.noreply.github.com>
2024-12-13 10:58:01 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,14 @@
 import logging
+
 import sonrai.platform.aws.arn
 
-log = logging.getLogger()
-
 def run(ctx):
-  pass
+    arn = sonrai.platform.aws.arn.parse(ctx.resource_id)
+    vpcid = arn \
+        .assert_service("ec2") \
+        .assert_type("vpc") \
+        .resource
+    logging.info('Removing VPC ({}) from AWS'.format(vpcid))
+    ec2client = ctx.get_client().get('ec2', region=arn.region)
+    response = ec2client.delete_vpc(VpcId=vpcid)
+    logging.info('Removed VPC: {}'.format(response))
